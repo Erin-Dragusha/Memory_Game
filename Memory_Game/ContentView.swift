8b09a7bea55c7d8749f8e0d02d4ed6150b337c61
Erin Dragusha <dragushaerin@gmail.com>
2023-10-03 11:34:26 +0200

    Initial Commit

---
+++ Memory_Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memory_Game
+//
+//  Created by Erin Dragusha on 2023-10-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memory_Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory_Game
+//
+//  Created by Erin Dragusha on 2023-10-03.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
